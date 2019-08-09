cfc0231b48438f192f67b584370168aaf4a44f5b
Jeremy Hill <j.c.hillmail@gmail.com>
2019-08-09 13:12:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  
+  def roster
+    @roster = Hash.new 
+  end  
+  
+  def initialize(school)
+    @school = school
+  end
+  
+  def add_student(grade, name)
+    if 
+    @roster.keys.include?(grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  end  
+  
+  
+  
+ end
+  
